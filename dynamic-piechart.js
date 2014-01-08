d3b210d956827a3d3eaec9e019503fccaf446afd
Jonathan Carter <jcarter@str.com>
2014-01-08 16:57:42 -0600

    Rewritten piechart mimics functionality of demo.  Removed demo.
    * The sector and text animate on hover

--- dynamic-piechart.js
+++ dynamic-piechart.js
@@ -7,6 +7,8 @@
 Raphael.fn.pieChart = function (entries, cx, cy, r, stroke) {
     "use strict;"
     var me = this;
+    var rad = Math.PI / 180;
+
     me.cx = cx || 350;
     me.cy = cy || 350;
     me.r = r || 200;
@@ -17,7 +19,6 @@ Raphael.fn.pieChart = function (entries, cx, cy, r, stroke) {
 
     function draw_sector(startAngle, endAngle, params){
 
-        var rad = Math.PI / 180;
         var x1 = me.cx + me.r * Math.cos(-startAngle * rad);
         var x2 = me.cx + me.r * Math.cos(-endAngle * rad);
         var y1 = me.cy + me.r * Math.sin(-startAngle * rad);
@@ -31,14 +32,28 @@ Raphael.fn.pieChart = function (entries, cx, cy, r, stroke) {
         var angle = 0;
         var total = me.entries.reduce(function(x,y){ return {value: x.value + y.value} }).value;
         var start = 0;
+        var duration = 500;
 
         me.entries.forEach(function(entry){
             var angleplus = 360 * entry.value / total;
+            var popangle = angle + (angleplus / 2);
             var color = Raphael.hsb(start, 0.75, 1);
             var delta = 30;
             var bcolor = Raphael.hsb(start, 1, 1);
 
-            draw_sector( angle, angle + angleplus, {fill: "90-" + bcolor + "-" + color, stroke: me.stroke, "stroke-width": 3});
+            var p = draw_sector( angle, angle + angleplus, {fill: "90-" + bcolor + "-" + color, stroke: me.stroke, "stroke-width": 3});
+            var txt = me.text(me.cx + (me.r + delta + 55) * Math.cos(-popangle * rad), me.cy + (me.r + delta + 25) * Math.sin(-popangle * rad), entry.label + "(" + entry.value + ")")
+                    .attr({fill: bcolor, stroke: "none", opacity: 0, "font-size": 20});
+
+            p.mouseover(function () {
+                p.stop().animate({transform: "s1.1 1.1 " + me.cx + " " + me.cy}, duration, "elastic");
+                txt.stop().animate({opacity: 1}, duration, "elastic");
+
+            }).mouseout(function () {
+                p.stop().animate({transform: ""}, duration, "elastic");
+                txt.stop().animate({opacity: 0}, duration);
+
+            });
 
             angle += angleplus;
             start += 0.1;
@@ -50,80 +65,4 @@ Raphael.fn.pieChart = function (entries, cx, cy, r, stroke) {
 
     return me;
 
-};
-
-Raphael.fn.demo_pieChart = function (cx, cy, r, values, labels, stroke) {
-
-    var paper = this;
-    var rad = Math.PI / 180;
-    var chart = this.set();
-
-    function make_sector(cx, cy, r, startAngle, endAngle, params) {
-        var x1 = cx + r * Math.cos(-startAngle * rad);
-        var x2 = cx + r * Math.cos(-endAngle * rad);
-        var y1 = cy + r * Math.sin(-startAngle * rad);
-        var y2 = cy + r * Math.sin(-endAngle * rad);
-
-        var patharr = ["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]
-        return paper.path(patharr).attr(params);
-    }
-
-    function draw(){
-
-        var angle = 0;
-        var total = 0;
-        var start = 0;
-
-            process = function (j) {
-                var value = values[j];
-                var angleplus = 360 * value / total;
-                var popangle = angle + (angleplus / 2);
-                var color = Raphael.hsb(start, .75, 1);
-                var ms = 500;
-                var delta = 30;
-                var bcolor = Raphael.hsb(start, 1, 1);
-
-                var p = make_sector(cx, cy, r, angle, angle + angleplus, {fill: "90-" + bcolor + "-" + color, stroke: stroke, "stroke-width": 3});
-                var txt = paper.text(cx + (r + delta + 55) * Math.cos(-popangle * rad), cy + (r + delta + 25) * Math.sin(-popangle * rad), labels[j] + "(" + values[j] + ")").attr({fill: bcolor, stroke: "none", opacity: 0, "font-size": 20});
-
-                p.mouseover(function () {
-                    p.stop().animate({transform: "s1.1 1.1 " + cx + " " + cy}, ms, "elastic");
-                    txt.stop().animate({opacity: 1}, ms, "elastic");
-
-                }).mouseout(function () {
-                    p.stop().animate({transform: ""}, ms, "elastic");
-                    txt.stop().animate({opacity: 0}, ms);
-
-                });
-
-                angle += angleplus;
-                chart.push(p);
-                chart.push(txt);
-                start += .1;
-            };
-
-        for (var i = 0, ii = values.length; i < ii; i++) {
-            total += values[i];
-        }
-
-        for (i = 0; i < ii; i++) {
-            process(i);
-        }
-    };
-
-    this.add_sector = function(value, label){
-        values.push(value);
-        labels.push(label + " (" + value + ")");
-        draw();
-    };
-
-    this.remove_sector = function(index){
-        [values, labels].forEach(function(arr){
-            arr.pop();
-        });
-        draw();
-    };
-
-    draw();
-    return this;
 };
