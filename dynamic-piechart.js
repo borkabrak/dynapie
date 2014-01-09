69df92b9a4018e02834c82f69696b5c91ad550ec
Jonathan Carter <jcarter@str.com>
2014-01-09 16:28:13 -0600

    Moved private functions to bottom of object

--- dynamic-piechart.js
+++ dynamic-piechart.js
@@ -18,36 +18,24 @@ Raphael.fn.pieChart = function (entries, cx, cy, r, stroke) {
 
     me.entries = entries; // data points.  Each should have 'label' and 'value'
 
-    // Return a point ( {x:<val>, y:<val>} ), a certain distance from the center at a certain angle
-    // Units of distance are radii.  (i.e., 0 is the center and 1 is on the circumference)
-    function get_point(distance_from_center, angle) {
-        return {
-            x: me.cx + me.r * distance_from_center * Math.cos(-angle * rad),
-            y: me.cy + me.r * distance_from_center * Math.sin(-angle * rad)
+    // Add a sector
+    me.add = function(label, value) {
+        if (label.length <= 0 || value.length <= 0) { return null };
+        me.entries.push({label: label, value: parseInt(value)});
+        me.draw();
+        return me.entries;
+    };
+
+    // Remove a sector
+    me.remove = function() {
+        if (me.entries.length < 3) { 
+            log("Can't remove entry.  Chart must have at least two entries.");
+            return null 
         };
-    };
 
-    // Draw and return a sector
-    function make_sector(startAngle, endAngle, params){
-
-        var point1 = get_point(1, startAngle);
-        var point2 = get_point(1, endAngle);
-
-        var patharr = ["M", me.cx, me.cy, "L", point1.x, point1.y, "A", me.r, me.r, 0, +(endAngle - startAngle > 180), 0, point2.x, point2.y, "z"]
-        return me.path( patharr ).attr(params);
-    };
-
-    // Draw and return a text element
-    function make_text(string, where) {
-        return me.text(
-            where.x,
-            where.y,
-            string
-        ).attr({
-            fill: "#000",
-            stroke: "none",
-            "font-size": 20,
-        });
+        var entry = me.entries.pop();
+        me.draw();
+        return entry;
     };
 
     // Draw the whole chart.  Return the set of elements
@@ -97,26 +85,41 @@ Raphael.fn.pieChart = function (entries, cx, cy, r, stroke) {
         });
     };
 
-    me.add = function(label, value) {
-        if (label.length <= 0 || value.length <= 0) { return null };
-        me.entries.push({label: label, value: parseInt(value)});
-        me.draw();
-        return me.entries;
-    };
-
-    me.remove = function() {
-        if (me.entries.length < 3) { 
-            log("Can't remove entry.  Chart must have at least two entries.");
-            return null 
-        };
-
-        var entry = me.entries.pop();
-        me.draw();
-        return entry;
-    };
-
     me.draw();
-
     return me;
+
+
+    // PRIVATE FUNCTIONS
+    //====================
+
+    // Return a point ( {x:<val>, y:<val>} ), a certain distance from the center at a certain angle
+    // Units of distance are radii.  (i.e., 0 is the center and 1 is on the circumference)
+    function get_point(distance_from_center, angle) {
+        return {
+            x: me.cx + me.r * distance_from_center * Math.cos(-angle * rad),
+            y: me.cy + me.r * distance_from_center * Math.sin(-angle * rad)
+        };
+    };
+
+    // Draw and return a sector
+    function make_sector(startAngle, endAngle, params){
+        var point1 = get_point(1, startAngle);
+        var point2 = get_point(1, endAngle);
+        var patharr = ["M", me.cx, me.cy, "L", point1.x, point1.y, "A", me.r, me.r, 0, +(endAngle - startAngle > 180), 0, point2.x, point2.y, "z"]
+        return me.path( patharr ).attr(params);
+    };
+
+    // Draw and return a text element
+    function make_text(string, where) {
+        return me.text(
+            where.x,
+            where.y,
+            string
+        ).attr({
+            fill: "#000",
+            stroke: "none",
+            "font-size": 20,
+        });
+    };
 
 };
